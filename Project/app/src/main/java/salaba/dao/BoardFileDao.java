783b38c2413a0444aa735d42705b335e192ddcae
Sejin PARK <150787016+SejinPrk@users.noreply.github.com>
2024-04-12 14:32:46 +0900

    .

--- Project/app/src/main/java/salaba/dao/BoardFileDao.java
+++ Project/app/src/main/java/salaba/dao/BoardFileDao.java
@@ -5,18 +5,19 @@ import org.apache.ibatis.annotations.Mapper;
 import salaba.vo.BoardFile;
 
 @Mapper
-public interface BoardFileDao { // 게시판 첨부파일 인터페이스
+public interface BoardFileDao { // 게시판, 신고 첨부파일 인터페이스
 
-  void add(BoardFile file);
+  void add(BoardFile file); // 파일 추가
 
-  int addAll(List<BoardFile> files);
+  int addAll(List<BoardFile> files); // 파일 전체 추가
 
-  int delete(int no);
+  int delete(int fileNo); // 파일 삭제
 
-  int deleteAll(int communityNo);
+  int deleteAll(int boardNo); // 파일 전체 삭제
 
-  BoardFile findByNo(int no);
+  BoardFile findByNo(int fileNo); // 파일 찾기
 
-  List<BoardFile> findAllByCommnunityNo(int communityNo);
-  List<BoardFile> findAllByReviewNo(int reviewNo);
+  List<BoardFile> findAllByBoardNo(int boardNo); // 게시판 번호로 게시판 첨부파일 찾기
+
+  List<BoardFile> findAllByReportNo(int reportNo); // 신고 번호로 신고 첨부파일 찾기
 }
--- Project/app/src/main/java/salaba/vo/BoardFile.java
+++ Project/app/src/main/java/salaba/vo/BoardFile.java
@@ -1,5 +1,6 @@
 package salaba.vo;
 
+import java.io.Serializable;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -8,14 +9,16 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor // Mybatis 가 사용할 기본 생성자를 만들어야 한다.
 @AllArgsConstructor
 @Builder
-@Data
-public class BoardFile { // 첨부파일
+@Data // 클래스 내의 모든 필드에 대한 Getter와 Setter 메서드를 생성
+public class BoardFile implements Serializable { // 게시판, 신고 첨부파일
 
-  private int no; // 파일 번호
-  private String filePath; // 파일 경로
-  private int communityNo; // 정보공유,자유 게시판 번호
-  private int reviewNo; // 후기 게시판 번호
-  private char oriFileName; // 원본 파일명
-  private char uuidFileName; // UUID 파일명
+  private static final long serialVersionUID = 100L;
+
+  private int fileNo; // 파일 번호
+  private int boardNo; // 게시판 번호
+  private String oriFileName; // 원본 파일명
+  private String uuidFileName; // UUID 파일명
+  private int order; // 사진 순서
+
 
 }
